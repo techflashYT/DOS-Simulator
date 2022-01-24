63c6cc2eefd68cbc2e8e292ee1cf5609a10697a6
sanikdah <michaelfgarofalo@gmail.com>
2022-01-23 17:20:13 -0700

    Works now
    Add a LOT of code to make the directory be created.

--- src/FileActions.cpp
+++ src/FileActions.cpp
@@ -1,25 +1,98 @@
 #include "inc/FileActions.h"
+string pathString = "";
 
-bool checkDataDir() {
+
+bool checkDataDir(string arg1) {
+	string username;
 #ifdef _WIN32
-    string username = getenv("USERNAME");
-    string pathString = "C:\\Users\\";
-    pathString.append(username);
-    pathString.append("\\Sanikdah Software\\DOS-Simulator");
+	struct stat b;
+	string a = "C:\\Temp";
+	if (stat(a.c_str(), &b) != 0) {
+		system("mkdir C:\\Temp");
+	}
+	ofstream script("C:\\Temp\\script.bat");
+	script << "@echo off\r\necho %username% > C:\\Temp\\username.txt";
+	script.close();
+	system("C:\\Temp\\script.bat");
+	ifstream unamefile("C:\\Temp\\username.txt");
+	if (unamefile.is_open()) {
+		unamefile >> username;
+	}
+	unamefile.close();
+	system("rmdir /Q /S C:\\Temp");
+	pathString = "C:\\Users\\";
+	pathString.append(username);
+	pathString.append("\\Appdata\\Roaming\\Sanikdah Software");
+	a = pathString;
+	if (stat(a.c_str(), &b) != 0) {
+		string cmd = "mkdir \"";
+		cmd.append(pathString);
+		cmd.append("\"");
+		char cmd1[100];
+		strcpy_s(cmd1, cmd.c_str());
+		system(cmd1);
+	}
+	pathString.append("\\DOS Simulator");
+	a = pathString;
+	if (stat(a.c_str(), &b) != 0) {
+		string cmd = "mkdir \"";
+		cmd.append(pathString);
+		cmd.append("\"");
+		char cmd1[100];
+		strcpy_s(cmd1, cmd.c_str());
+		system(cmd1);
+	}
+#else
+	pathString = "/home/";
+	cout << "What is your usermame?\r\n";
+	cin >> username;
+	if (username == "root" && !(arg1 == "--use-root")) {
+		cerr << "\r\nIt is highly recommended not to run the application as root.  For more information, see https://https://github.com/sanikdah/DOS-Simulator/wiki/%5BLinux%5D-Running-as-root .  \r\n \
+		If it is absolutely necessary, run with the --use-root command line flag.  Exiting...\r\n";
+		std::exit(1);
+	}
+	else if (username == "root" && arg1 == "--use-root") {
+		cerr << "\r\nIt is highly recommended not to run the application as root.  For more information, see https://https://github.com/sanikdah/DOS-Simulator/wiki/%5BLinux%5D-Running-as-root . \r\n \
+		since the --use-root flag is present, we will use this anyways, despite the risks.";
+		pathString = "/root/Sanikdah Software/";
+	}
+	else {
+		pathString.append(username);
+		pathString.append("/Sanikdah Software/");
+	}
 #endif
 
-    path path(pathString); // Constructing the path from a string is possible.
-    error_code ec; // For using the non-throwing overloads of functions below.
-    if (is_directory(path, ec)) {
-        // Process a directory.
-    }
-    if (ec) { // Optional handling of possible errors
-        cerr << "Error in is_directory: " << ec.message();
-    }
-    if (is_regular_file(path, ec)) {
-        // Process a regular file.
-    }
-    if (ec) { // Optional handling of possible errors. Usage of the same ec object works since fs functions are calling ec.clear() if no errors occur.
-        cerr << "Error in is_regular_file: " << ec.message();
-    }
+	path path(pathString); // Constructing the path from a string is possible.
+	error_code ec; // For using the non-throwing overloads of functions below.
+	if (is_directory(path, ec)) {
+		return true;
+	}
+	if (is_regular_file(path, ec)) {
+		cerr << "The data directory is a file!  Deleting it...\r\n";
+#ifdef _WIN32
+		string cmd = "del \"";
+		cmd.append(pathString);
+		cmd.append("\"");
+		char cmd1[100];
+		strcpy_s(cmd1, cmd.c_str());
+		system(cmd1);
+		cout << "Done!  Please restart the program!\r\n";
+		system("pause");
+		std::exit(0);
+#else
+		string cmd = "rm \"";
+		cmd.append(pathString);
+		cmd.append("\"");
+		char cmd1[100];
+		strcpy_s(cmd1, cmd.c_str());
+		system(cmd1);
+		cout << "Done!  Please restart the program!\r\nPress any key to continue...";
+		system("read -n1 key");
+		std::exit(0);
+#endif
+	}
+	if (ec) { // Optional handling of possible errors. Usage of the same ec object works since fs functions are calling ec.clear() if no errors occur.
+		cerr << "An unknown error occurred reading the data directory, the error is:\r\n" << ec.message() << "\r\nexiting...";
+		std::exit(1);
+	}
 }
--- src/inc/FileActions.h
+++ src/inc/FileActions.h
@@ -1,8 +1,13 @@
 #pragma once
 
+#include "defines.h"
+
+
 #include <filesystem>
 #include <iostream>
 #include <string>
+#include <sys/stat.h>
+#include <fstream>
 
 
 //#include <windows.h>
@@ -13,3 +18,7 @@ using std::filesystem::path;
 using std::error_code;
 using std::filesystem::is_directory;
 using std::cerr;
+using std::cout;
+using std::cin;
+using std::ifstream;
+using std::ofstream;
