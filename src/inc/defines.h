8b3a98ef53d99ba60b658c676bc62abbf88d5f7c
Techflash <michaelfgarofalo@gmail.com>
2022-01-27 23:27:56 -0800

    Add IDs & Error string
    Add return IDs and an error string.

--- src/inc/defines.h
+++ src/inc/defines.h
@@ -1,3 +1,32 @@
 #pragma once
 
 #define ver 0.1
+
+#define MAX_SHELLS 25
+#define MAX_COMMAND_LENGTH 100
+
+#define SUCCESS							0
+// Errors
+#define ERROR_INIT			          101
+#define ERROR_CURL_SETUP_FAIL         102
+#define ERROR_ASSET_DOWNLOAD          103
+#define ERROR_MAIN_DIR_CORRUPT        104
+#define ERROR_DRIVE_DIRS_CORRUPT      105
+#define ERROR_ASSETS_NOT_DOWNLOADED   106
+#define ERROR_DIRECTORY_NOT_CHANGED   107
+#define ERROR_FELL_THROUGH_IF_ELSE    108
+
+
+
+
+// Warnings
+#define WARNING_NONEXISTENT_DIRECTORY 209
+
+
+
+
+
+// Error Strings
+#define ERROR_DIRECTORY_NOT_FOUND "The directory does not exist.\r\n"
+
+typedef unsigned short int USI;
